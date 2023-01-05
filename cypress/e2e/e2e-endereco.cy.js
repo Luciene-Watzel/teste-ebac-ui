927d48fa083e38225eafe4ac3515d01481979670
Luciene Watzel <lu.watzel@gmail.com>
2023-01-05 20:20:38 -0300

    usando massa de dados

--- cypress/e2e/e2e-endereco.cy.js
+++ cypress/e2e/e2e-endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 beforeEach(() => {
@@ -9,8 +11,31 @@ beforeEach(() => {
     
 });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso ', () => {
+    it('Deve fazer cadastro de faturamento com sucesso ', () => {
         
-        // cadastro de endereço
+        EnderecoPage.editarEnderecoFaturamento('Maria', 'Souza', 'Avon', 'Brasil', 'Avenida Interlagos', '2000', 'São Paulo', 'São Paulo', '04750000','11999998888', 'maria@empresa.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+        
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email,
+             )
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
+    });
+
+    
+        
+    
 });
